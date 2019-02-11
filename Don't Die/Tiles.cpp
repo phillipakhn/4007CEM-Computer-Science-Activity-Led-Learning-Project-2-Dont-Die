7082281997e9bb270cc7571de101512dc10825ef
Alastair Holland (hollan84) <hollan84@coventry.ac.uk>
2019-02-11 17:09:01 +0000

    Tiles

--- Don't Die/Tiles.cpp
+++ Don't Die/Tiles.cpp
@@ -1,9 +1,18 @@
 #include "Tiles.h"
+#include "Dot.h"
 
 
 
-Tiles::Tiles()
+Tiles::Tiles(int x, int y, int tileType)
 {
+	box.x = x;
+	box.y = y;
+
+	box.w = TILE_WIDTH;
+	box.h = TILE.HEIGHT;
+
+	type = tileType;
+
 }
 
 
