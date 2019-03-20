0bdfabac888d132eecfc85ce36ec2577d072a85b
Kieran Dhir (dhirk) <dhirk@coventry.ac.uk>
2019-03-20 02:01:22 +0000

    Delete drawTiles.cpp

--- drawTiles.cpp
+++
@@ -1,9 +0,0 @@
-#include "drawTiles.h"
-
-drawTiles::~drawTiles()
-{
-}
-
-void drawTiles::Draw(SDL_Texture * texture, SDL_Rect src, SDL_Rect dest) {
-	SDL_RenderCopy(Game::renderer, texture, &src, &dest);
-}
