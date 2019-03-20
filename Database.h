9cd570e0d9ef6aa057f92c0f941aa2d5299438f7
Kieran Dhir (dhirk) <dhirk@coventry.ac.uk>
2019-03-20 01:59:48 +0000

    Delete Database.h

--- Database.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "libsqlite.hpp"
-#include <vector>
-
-int sqlSelect(char Axis, int play);
-int sqlSelectMap(char Axis, int play);
-int sqlUpdate(int x, int y);
-int sqlUpdateMap(int xMap, int yMap);
-std::vector<int> Position();
