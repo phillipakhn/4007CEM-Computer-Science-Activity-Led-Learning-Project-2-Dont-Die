10b2118b981f2b6ed2ed8d1fdae6f160f99d692e
Kieran Dhir (dhirk) <dhirk@coventry.ac.uk>
2019-03-20 02:01:28 +0000

    Delete TextureController.h

--- TextureController.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Game.h"
-
-class TextureController {
-
-public:
-	static SDL_Texture* LoadTexture(const char* filename, SDL_Renderer* render);
-};
-// Simple texture loader youtube 
